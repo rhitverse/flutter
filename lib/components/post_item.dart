ca7983377fd0efeda9a939f846d211efe005d845
rhitverse <rohitsainii.work@gmail.com>
2025-09-24 19:30:04 +0530

    flutter3

--- lib/components/post_item.dart
+++ lib/components/post_item.dart
@@ -2,17 +2,33 @@ import 'package:coading/styles/app_text.dart';
 import 'package:flutter/material.dart';
 
 class PostItem extends StatelessWidget {
-  const PostItem({super.key});
+  final String user;
+  const PostItem({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
     MediaQuery.of(context).size;
-     return Row(
-      children: [
-        Image.asset('assets/temp/user1.png', width: 40, height: 40),
-        SizedBox(width: 16),
-        Text("Steve Harrinton", style: AppText.subtitle3),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 12),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset('assets/temp/user1.png', width: 40, height: 40),
+              SizedBox(width: 16),
+              Text(
+                user, style: AppText.subtitle3),
+            ],
+          ),
+          SizedBox(height: 12),
+          Image.asset('assets/temp/post1.jpg'),
+          SizedBox(height: 12),
+          Text(
+            'The Rising Sun reminds me of you bring light to my darkest days💖',
+            style: AppText.subtitle3,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,26 +3,37 @@ import 'package:coading/styles/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<String> users = [];
 
   @override
   Widget build(BuildContext context) {
+    mockUsersFromServer();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.background,
         centerTitle: false,
         title: Text('5minuteflutter', style: TextStyle(color: Colors.white)),
-        actions: [Icon(Icons.add_ic_call_outlined)],
+        actions: [Icon(Icons.location_on_outlined)],
       ),
-      body: ListView(children: mockUsersFromServer()),
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return PostItem(user: users[index]);
+        },
+        itemCount: users.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            height: 24,
+          );
+        },
+      ),
     );
   }
 
-  List<Widget> mockUsersFromServer() {
-    List<Widget> users = [];
-    for (var i = 0; i < 1000; i++) {
-      users.add(PostItem());
+  mockUsersFromServer() {
+    for (var i = 0; i < 100; i++) {
+      users.add('User number $i');
     }
-    return users;
   }
 }
